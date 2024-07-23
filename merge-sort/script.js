5881e3cee7f17c6b36a5e7dc2aa420c1bc0d2222
Nikhilesh-Sanka <sankanikhilesh2006.svc@gmail.com>
2024-07-23 15:48:57 +0530

    corrected my merge-sort Function

--- merge-sort/script.js
+++ merge-sort/script.js
@@ -1,16 +1,39 @@
-function floatToTop(array, indexToStart = 0) {
-  if (indexToStart === array.length - 1) {
+function mergeAndSortArray(
+  array,
+  array1 = array.slice(0, Math.floor(array.length / 2)),
+  array2 = array.slice(Math.floor(array.length / 2), array.length)
+) {
+  if (array.length === 1) {
     return array;
   } else {
-    for (let i = array.length - 1; i >= 0; i--) {
-      let a = array[i];
-      let b = array[i + 1];
-      if (a < b) {
-        array[i + 1] = a;
-        array[i] = b;
-      }
-    }
-    return floatToTop(array, indexToStart + 1);
+    array1 = mergeAndSortArray(array1);
+    array2 = mergeAndSortArray(array2);
+    return mergeSortedArray(array1, array2);
   }
 }
-console.log(floatToTop([9, 0, 8, 77, 63, 87, 9, 8]));
+
+function mergeSortedArray(array1, array2) {
+  let pointerOne = 0;
+  let pointerTwo = 0;
+  let mergedArray = [];
+  while (pointerOne < array1.length && pointerTwo < array2.length) {
+    if (array1[pointerOne] <= array2[pointerTwo]) {
+      mergedArray.push(array1[pointerOne]);
+      pointerOne++;
+    } else {
+      mergedArray.push(array2[pointerTwo]);
+      pointerTwo++;
+    }
+  }
+  while (pointerOne === array1.length && pointerTwo < array2.length) {
+    mergedArray.push(array2[pointerTwo]);
+    pointerTwo++;
+  }
+  while (pointerOne < array1.length && pointerTwo === array2.length) {
+    mergedArray.push(array1[pointerOne]);
+    pointerOne++;
+  }
+  return mergedArray;
+}
+let testArray = [9, 2, 14, 2, 3, 4, 10, 7, 65, 21];
+console.log(mergeAndSortArray(testArray));
